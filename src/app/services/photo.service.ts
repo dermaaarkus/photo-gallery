f66ce91b0e98883ffcad84918ad85a7d3c7527a3
Markus Faßbender <markus.fassbender@appsfactory.de>
2019-09-30 12:48:48 +0200

    add photoservice to fill galery

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
 
-  constructor() { }
+  public photos: Photo[] = [];
+
+  constructor(private camera: Camera, public photoService: PhotoService) { }
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.photos.unshift({
+        data: 'data:image/jpeg;base64,' + imageData
+      });
+    }, (error) => {
+      console.log("Camera issue: " + error)
+    });
+  }
+}
+
+class Photo {
+  data: any;
 }
