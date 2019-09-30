e502a095f5befda4f51fdfef110566d138d630e7
Markus Faßbender <markus.fassbender@appsfactory.de>
2019-09-30 13:08:02 +0200

    persist photos

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Storage } from "@ionic/storage";
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class PhotoService {
 
   public photos: Photo[] = [];
 
-  constructor(private camera: Camera, public photoService: PhotoService) { }
+  constructor(private camera: Camera, private storage: Storage) { }
 
   takePicture() {
     const options: CameraOptions = {
@@ -22,8 +23,16 @@ export class PhotoService {
       this.photos.unshift({
         data: 'data:image/jpeg;base64,' + imageData
       });
+
+      this.storage.set('photos', this.photos);
     }, (error) => {
       console.log("Camera issue: " + error)
+    });
+  }
+
+  loadSaved() {
+    this.storage.get('photos').then((photos) => {
+      this.photos = photos || [];
     });
   }
 }
